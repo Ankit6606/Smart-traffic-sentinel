0b9c25adeff91252f3614307ef879008013e4680
Debanjan Dhara <dd.debanjandhara@gmail.com>
2023-09-11 22:54:55 +0530

    Version 1.0 -- all three tables added; violations are grtting added

--- traffic-application-2/models/vehicle.js
+++ traffic-application-2/models/vehicle.js
@@ -15,15 +15,71 @@ const Vehicle = {
     
     checkPollutionToday: (number_plate) => {
       const today = new Date().toISOString().split('T')[0]; // Get today's date in 'YYYY-MM-DD' format
-      
+    
       return new Promise((resolve, reject) => {
         db.get('SELECT * FROM vehicles WHERE number_plate = ? AND pollution_status > ?', [number_plate, today], (err, row) => {
           if (err) return reject(err);
-          resolve(row ? 1 : 0);
+    
+          if (row) {
+            // Pollution is up to date, resolve with 1
+            resolve(1);
+          } else {
+            // Pollution is not up to date, insert a violation record
+            db.run('INSERT INTO violations (vehicle_id, violation_type, violation_date) SELECT vehicle_id, "pollution", ? FROM vehicles WHERE number_plate = ?', [today, number_plate], (err) => {
+              if (err) return reject(err);
+              resolve(0); // Resolve with 0 indicating a violation
+            });
+          }
         });
       });
-    }
-  };
+    },
+
+    // Function to check if tax is up to date
+checkTaxToday: (number_plate) => {
+  const today = new Date().toISOString().split('T')[0]; // Get today's date in 'YYYY-MM-DD' format
+
+  return new Promise((resolve, reject) => {
+    db.get('SELECT * FROM vehicles WHERE number_plate = ? AND tax_status > ?', [number_plate, today], (err, row) => {
+      if (err) return reject(err);
+
+      if (row) {
+        // Tax is up to date, resolve with 1
+        resolve(1);
+      } else {
+        // Tax is not up to date, insert a violation record
+        db.run('INSERT INTO violations (vehicle_id, violation_type, violation_date) SELECT vehicle_id, "tax", ? FROM vehicles WHERE number_plate = ?', [today, number_plate], (err) => {
+          if (err) return reject(err);
+          resolve(0); // Resolve with 0 indicating a violation
+        });
+      }
+    });
+  });
+},
+
+// Function to check if insurance is up to date
+checkInsuranceToday: (number_plate) => {
+  const today = new Date().toISOString().split('T')[0]; // Get today's date in 'YYYY-MM-DD' format
+
+  return new Promise((resolve, reject) => {
+    db.get('SELECT * FROM vehicles WHERE number_plate = ? AND insurance_status > ?', [number_plate, today], (err, row) => {
+      if (err) return reject(err);
+
+      if (row) {
+        // Insurance is up to date, resolve with 1
+        resolve(1);
+      } else {
+        // Insurance is not up to date, insert a violation record
+        db.run('INSERT INTO violations (vehicle_id, violation_type, violation_date) SELECT vehicle_id, "insurance", ? FROM vehicles WHERE number_plate = ?', [today, number_plate], (err) => {
+          if (err) return reject(err);
+          resolve(0); // Resolve with 0 indicating a violation
+        });
+      }
+    });
+  });
+}
+
+    
+};
   
 
 module.exports = Vehicle;
--- traffic-application-2/routes/index.js
+++ traffic-application-2/routes/index.js
@@ -1,13 +1,23 @@
-// routes/index.js
 const express = require('express');
 const router = express.Router();
 const Vehicle = require('../models/vehicle');
 
 router.get('/vehicles', async (req, res) => {
   try {
-    const vehicles = await Vehicle.getAll();
-    res.json(vehicles);
+    const numberPlate = 'XYZ789'; // Replace with the desired number plate
+    const [pollutionResult, taxResult, insuranceResult] = await Promise.all([
+      Vehicle.checkPollutionToday(numberPlate),
+      Vehicle.checkTaxToday(numberPlate),
+      Vehicle.checkInsuranceToday(numberPlate),
+    ]);
+
+    res.json({
+      pollutionStatus: pollutionResult === 1 ? 'Up to date' : 'Violation',
+      taxStatus: taxResult === 1 ? 'Up to date' : 'Violation',
+      insuranceStatus: insuranceResult === 1 ? 'Up to date' : 'Violation',
+    });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
